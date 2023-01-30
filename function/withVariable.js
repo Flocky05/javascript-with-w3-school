230cfa58dcc1da2f276f83cb94cecd3d457bdfdf
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-30 14:22:29 +0600

    added function that use a varibale

--- function/withVariable.js
+++ function/withVariable.js
@@ -4,3 +4,10 @@ function sum(a, b) {
   console.log("The summation of two numbers is " + (a + b));
 }
 sum(3, 5);
+
+// when we declar a variable outsite of funciton that time it works as..
+
+function run(a, b) {
+  console.log(a + b);
+}
+let x = run(10, 20);
