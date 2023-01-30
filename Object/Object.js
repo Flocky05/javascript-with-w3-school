dd13471acbb17e7bf9a0c8b17aa5b6be3b110aa6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-31 01:39:07 +0600

    added a norlmal object in js

---
+++ Object/Object.js
@@ -0,0 +1,12 @@
+// At frist if try to use Object in javascript , You have to know about the rules of Object in js. Object like a function but not like a function. A object have / has some parameeter, which is used like a variable. Normaly we start a type of data then use name then= + { each of variable : data} ,
+
+const car = {
+  name: "BMW",
+  model: "500",
+  weight: "850 kg",
+  color: "white",
+};
+
+console.log(car.name, car.model);
+
+//  we can also use insite the object a function.
--- function/withVariable.js
+++ function/withVariable.js
@@ -1,16 +1,16 @@
 //rules: In frst perameter have one or more variable that can be changeable.
 
-// function sum(a, b) {
-//   console.log("The summation of two numbers is " + (a + b));
-// }
-// sum(3, 5);
+function sum(a, b) {
+  console.log("The summation of two numbers is " + (a + b));
+}
+sum(3, 5);
 
 // when we declar a variable outsite of funciton that time it works as..
 
-// function run(a, b) {
-//   console.log(a + b);
-// }
-// let x = run(10, 20);
+function run(a, b) {
+  console.log(a + b);
+}
+let x = run(10, 20);
 
 // when we use variable insite function
 
