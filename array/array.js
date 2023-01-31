1a266bbf3a0deb7def979b81318baa16238a42fe
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-31 13:38:50 +0600

    length the array

--- array/array.js
+++ array/array.js
@@ -1,3 +1,7 @@
 // at frist use data type and name the array then use "=" sign and use [ insite ellemnt]
 const friendsName = ["mehedi", "emon", "siam", "krishno", "alamin"];
 console.log(friendsName);
+
+// length the array
+
+console.log(friendsName.length);
