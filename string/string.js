4454874eafef2f94ebd8f58c6b28c0ba5b44e442
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-01 14:47:56 +0600

    inside of string use double coute

---
+++ string/string.js
@@ -0,0 +1,8 @@
+// length of string
+const text = "rakibulIslam";
+console.log(text.length);
+
+//some time we have to need " " in side the string that time we can use before the " " use \ like:
+
+const sentence = 'I like to  go "Dhaka" any time';
+console.log(sentence, sentence.length);
