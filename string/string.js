b94f9f94274d3eb913c6c9c23778cf83566d6ae1
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-01 14:57:51 +0600

    compare two object in string

--- string/string.js
+++ string/string.js
@@ -6,3 +6,11 @@ console.log(text.length);
 
 const sentence = 'I like to  go "Dhaka" any time';
 console.log(sentence, sentence.length);
+
+// some time in string we compare two more object with (==) or (===) all time output will be flase
+
+let x = new String("rakibul");
+let y = new String("rakibul");
+
+console.log(x == y);
+console.log(x === y);
