679176ab7eee235429b473aab14d0e45de5b7c8a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-02 02:15:00 +0600

    use slice method in string

--- string/string.js
+++ string/string.js
@@ -9,14 +9,24 @@ console.log(sentence, sentence.length);
 
 // some time in string we compare two more object with (==) or (===) all time output will be flase
 
-let x = new String("rakibul");
-let y = new String("rakibul");
+// let x = new String("rakibul");
+// let y = new String("rakibul");
 
-console.log(x == y);
-console.log(x === y);
+// console.log(x == y);
+// console.log(x === y);
 
-// an object and a string can be compare
-let m = "rakibul";
-let n = new String("rakibul");
-console.log(m == n);
-console.log(m === n);
+// // an object and a string can be compare
+// let m = "rakibul";
+// let n = new String("rakibul");
+// console.log(m == n);
+// console.log(m === n);
+
+// JS String Methods
+// String slice(start,end), String substring(),String substr(),String replace(), String replaceAll(), String toUpperCase(), String toLowerCase(), String concat(), String trim(), String trimStart(), String trimEnd(), String padStart(), String padEnd(), String charAt() etc
+
+// use of slice method is slice(start,end)
+let friends = "mehedi,emon,siam,krishno,kauch,shanto";
+console.log(friends.slice(3, 17));
+//  if you use only one parameter that like as output will be like slice(0, end)
+
+console.log(friends.slice(7));
