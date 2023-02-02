04a7d2013b1652258d4c42f4ee92c574b2b98e3d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-02 15:07:21 +0600

    use index.Of

---
+++ string/stringSearch.js
@@ -0,0 +1,3 @@
+// every time counting start from 0 and output will show all time frist letter's number and each of letter is counted one by one.
+let text = "let's go to Dhaka";
+console.log(text.indexOf("let's"));
