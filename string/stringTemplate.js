b76edaa070a2161edc866ce6732c9678ddf55073
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-03 12:45:54 +0600

    added string templates

---
+++ string/stringTemplate.js
@@ -0,0 +1,5 @@
+// String templates used for reduce the complexcity the string, some time we can't use ',"",in the string, that's why we use templates, It's have some banifites
+
+const friends = "mehedi,emon";
+let sentence = `I am Md.Rakibul Islam and I have some ${friends} . I live in "Bangladesh"`;
+console.log(sentence);
