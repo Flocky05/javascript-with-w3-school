743366b12d978fc10b15a689e510f4af55503f6f
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-04 01:59:54 +0600

    added number type

---
+++ Number/number.js
@@ -0,0 +1,9 @@
+// In js number are normally 10 base and it normally used falting point and its has some problem like two floating number's sumation all time is not right in js.and its work correctly within 15 digits more then 15 digits it's not work properly. Its not to be need that declear a type of number.
+
+let x = 0.3;
+let y = 0.1;
+console.log(x + y);
+
+// add number and string  And the output is just concatanation with two x and z
+let z = "10";
+console.log(x + z);
