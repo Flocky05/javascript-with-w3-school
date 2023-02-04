dcdcfbe842a2a8523918bbcacdcc0bb1db515d6d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-04 20:05:19 +0600

    added bigNumber

---
+++ Number/Bignumber.js
@@ -0,0 +1,7 @@
+// some time we need to resolve some equation with like 5n/2 but that can't understant code. to solve that type of problem we have to need bignumber method
+
+let y = 5n;
+let x = Number(y) / 2;
+console.log(x);
+
+// if you try x=y/2 that time it's cann't show any result
