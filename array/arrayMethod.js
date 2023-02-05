72c83566da55692adfbd8551685bce58870b243e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-06 01:56:15 +0600

    added join methods

---
+++ array/arrayMethod.js
@@ -0,0 +1,5 @@
+// some array methods for join methods that methods normally used for join elements of array , you can use all time of things as you like
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(fruits.join("*"));
