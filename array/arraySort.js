7606660666daafaca96f36f5da553c8459ede824
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-06 14:32:18 +0600

    added numeric sort method

--- array/arraySort.js
+++ array/arraySort.js
@@ -6,3 +6,12 @@ console.log(x);
 //array reverse() means that last element to frist element printed
 let y = fruits.reverse();
 console.log(y);
+
+//numeric sort is a method that is used for numeric value like:
+
+const points = [12, 23, 34, 54, 1, 2, 3, 4, 5];
+x = points.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(x);
