e0f70002733f542c9ea156670a1ecc799592ac38
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-08 23:55:58 +0600

    added ceil and floor function

--- Math/math.js
+++ Math/math.js
@@ -1,2 +1,8 @@
-const x = Math.PI;
-console.log(x);
+// const x = Math.PI;
+// console.log(x);
+// ceil function in math
+
+let x = 10;
+let y = 9;
+console.log(Math.ceil(x / y));
+console.log(Math.floor(x / y));
