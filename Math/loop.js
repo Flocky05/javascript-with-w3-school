3443e6ab7ae5831fd8e9654f3361ff347f24ca5d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-10 15:03:49 +0600

    added trunc function

---
+++ Math/loop.js
@@ -0,0 +1,1 @@
+// loop mean same task have been done more then several time may you use it in for loop and while loop and do while loop etc
--- Math/math.js
+++ Math/math.js
@@ -6,3 +6,8 @@ let x = 10;
 let y = 9;
 console.log(Math.ceil(x / y));
 console.log(Math.floor(x / y));
+
+//turnc(x) means return a value that is integer value
+
+console.log(Math.trunc(4.9));
+console.log(Math.trunc(4.1));
