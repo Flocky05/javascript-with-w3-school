9423a03ae77ae3395eefc842674294e0350b1cd8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-11 20:34:20 +0600

    use fucntnion for ludu

--- problem solving/ludu.js
+++ problem solving/ludu.js
@@ -1,6 +1,14 @@
 // In ludu game how to show a (1-6)?
 
-let x = parseInt(6 * Math.random());
-if (x < 1) {
-  console.log(1);
-} else console.log(x);
+// let x = parseInt(6 * Math.random());
+// if (x < 1) {
+//   console.log(1);
+// } else console.log(x);
+
+// using function
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomNumber(1, 6));
