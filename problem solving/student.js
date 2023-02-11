5623ad8d8e33456b02e3cb20bd8bffe515ce4089
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-11 21:51:34 +0600

    leap year funtion

--- problem solving/student.js
+++ problem solving/student.js
@@ -10,3 +10,13 @@ const x = roll_number.sort(function (a, b) {
   return a - b;
 });
 console.log(x);
+
+// leap year
+
+function leap_year(year) {
+  if (year % 400 == 0 || (year % 100 !== 0 && year % 4 == 0)) {
+    console.log(`${year} is leap year`);
+  } else console.log(`${year} is not a leap year`);
+}
+
+leap_year(2026);
