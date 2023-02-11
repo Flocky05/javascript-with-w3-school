40d212028790b05b292f30fd7977176e5b177dee
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-11 21:43:55 +0600

    added roll_number in class

--- problem solving/student.js
+++ problem solving/student.js
@@ -3,3 +3,10 @@
 const students = ["mehedi", "rakibul", "emon", "siam", "krishno", "kauch"];
 
 console.log(students.sort());
+
+// roll in a serial
+const roll_number = [2, 1, 4, 45, 23, 56, 76];
+const x = roll_number.sort(function (a, b) {
+  return a - b;
+});
+console.log(x);
