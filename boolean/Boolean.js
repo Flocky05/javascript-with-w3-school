12d3b730228f8de46ea6d4b80815c1f179bdeea7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-14 16:29:15 +0600

    == return true

--- boolean/Boolean.js
+++ boolean/Boolean.js
@@ -1,8 +1,14 @@
 // boolean value means true or false value return
-let a = 10;
-let b = 19;
-let x = Booleanl(a > b);
-console.log(x);
+// let a = 10;
+// let b = 19;
+// let x = Booleanl(a > b);
+// console.log(x);
 // if answer is not right that time its show false and if answer is right then output will be true.
 
 // if any time the result /value is zero(0) then output will be false if any answer is -0 then it show output is false.
+
+// == use as here we just check the value, in js all type of value are string so here show right and and will be true
+
+let m = 6;
+let n = "6";
+console.log(m == n);
