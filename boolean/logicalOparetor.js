4e4c5bf54abd9214f6e459abdc3bfd7fd65a74f2
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-14 16:37:17 +0600

    added or operator

--- boolean/logicalOparetor.js
+++ boolean/logicalOparetor.js
@@ -4,3 +4,11 @@ let x = 58;
 if (x > 18) {
   console.log("I will go to pitha ussob");
 }
+
+// or operator
+
+let age = 17;
+
+if (age == 17 || age == 18 || age == 19) {
+  console.log("I am audalt boy");
+}
