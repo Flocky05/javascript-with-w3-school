8fcbff1f66fddd90be4b946dafc66aaca1a591ab
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-14 16:23:58 +0600

    boolean added

---
+++ boolean/Boolean.js
@@ -0,0 +1,8 @@
+// boolean value means true or false value return
+let a = 10;
+let b = 19;
+let x = Booleanl(a > b);
+console.log(x);
+// if answer is not right that time its show false and if answer is right then output will be true.
+
+// if any time the result /value is zero(0) then output will be false if any answer is -0 then it show output is false.
