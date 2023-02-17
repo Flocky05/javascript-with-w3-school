ff2a9535592922f24da60e1c98b7f5f0ccc96f5a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-17 23:46:14 +0600

    added for loop responsive

--- loops/forloop.js
+++ loops/forloop.js
@@ -1,3 +1,9 @@
-for (i = 0; i < 10; i++) {
-  console.log("My name is Md.Rakibul Islam");
+// for (i = 0; i < 10; i++) {
+//   console.log("My name is Md.Rakibul Islam");
+// }
+
+// print some number
+
+for (let i = 0; i < 5; i++) {
+  console.log(`The number is ${i}`);
 }
