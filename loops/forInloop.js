08d401892fea4f729657e097c57b40e93e79245a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-18 23:46:06 +0600

    added for in loop

---
+++ loops/forInloop.js
@@ -0,0 +1,10 @@
+//ruls of for in loop
+// for (key in object) {
+// code block to be executed
+// }
+
+const numbers = [45, 4, 9, 16, 25];
+
+for (let x in numbers) {
+  console.log(`${numbers[x]}`);
+}
