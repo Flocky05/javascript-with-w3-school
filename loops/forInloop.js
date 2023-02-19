a23fc3d2b95937ebb4a8c11b435947763a8f6005
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-19 23:48:29 +0600

    added for of loop

--- loops/forInloop.js
+++ loops/forInloop.js
@@ -8,3 +8,6 @@ const numbers = [45, 4, 9, 16, 25];
 for (let x in numbers) {
   console.log(`${numbers[x]}`);
 }
+
+const a = [1, 2, 3, 4, 5, 6];
+a.forEach();
---
+++ loops/forOfLoop.js
@@ -0,0 +1,9 @@
+// Syntax
+//for (variable of iterable) {
+// code block to be executed}
+//
+const cars = ["BMW", "Volvo", "Mini"];
+
+for (let x of cars) {
+  console.log(`${x}`);
+}
