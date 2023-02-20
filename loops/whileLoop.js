66e41fb8633748a9490cb786679ec498325f3244
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-20 14:44:40 +0600

    added basic while loop

---
+++ loops/whileLoop.js
@@ -0,0 +1,18 @@
+// syntax :
+
+//
+// while (condition) {
+//     // code block to be executed
+//   }
+
+// const x = ["mehedi", "emon", "siam"];
+// const i = 48;
+// while (i < 20) {
+//   console.log(`${x}`);
+// }
+
+let x = 0;
+while (x < 29) {
+  console.log(`The number is ${x}`);
+  x++;
+}
