f661ce4b7b17216577a9ff367d37aa26057e8295
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-22 01:36:38 +0600

    iterables adding

---
+++ loops/itarables.js
@@ -0,0 +1,13 @@
+// itarabel array,string
+
+// object is not itarable
+// const person={
+//     frist_name:"Rakibul",
+//     last_name:"Islam"
+// }
+
+const x = "Bangladesh";
+
+for (const i of x) {
+  console.log(i);
+}
