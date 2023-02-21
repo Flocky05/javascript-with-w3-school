0a260dd48399b084e2a6725c1a91f7ef072bef15
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-22 01:45:39 +0600

    added set in loops

---
+++ loops/set.js
@@ -0,0 +1,6 @@
+// A javaScript Set is a collection of unique values. each value can only occur once in a Set;
+
+// must be use Set
+
+const letters = new Set(["mehedi", "emon", "siam"]);
+console.log(letters);
