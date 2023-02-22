e21e9018d6fe02ccf5ef237e710e08b811dacf2a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-22 10:33:13 +0600

    null and array data type

--- type of/typeOf.js
+++ type of/typeOf.js
@@ -6,3 +6,12 @@ console.log(typeof c);
 // You have to must know that NaN is a type of number
 let x = NaN;
 console.log(typeof x);
+
+// array is a object type of
+
+let friends = [1, 2, 3, 4, 5];
+console.log(typeof friends);
+
+// null is also a Object type of data type
+let m = null;
+console.log(typeof m);
