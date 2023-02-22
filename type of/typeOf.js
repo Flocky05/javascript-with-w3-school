11caa1039c4b77595a93a3f3d0fa9e6cd65785f8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-22 10:30:57 +0600

    type of in js

---
+++ type of/typeOf.js
@@ -0,0 +1,8 @@
+const a = "Bangladesh";
+let b = "is a beautiful country";
+let c = a + " " + b;
+console.log(typeof c);
+
+// You have to must know that NaN is a type of number
+let x = NaN;
+console.log(typeof x);
