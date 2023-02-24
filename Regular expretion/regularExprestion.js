dbe35563a6d135bb4759b7a4582002cd9f6aa087
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-24 21:31:58 +0600

    added regularExp using search method

---
+++ Regular expretion/regularExprestion.js
@@ -0,0 +1,4 @@
+// why need to use regular exprestion in js? There are some resonse for using regular exprestion is js , like You have to need search rakiBul and that answer will be like any rakibul. But traditionly you find the answer using fixed string but some times we have to need like uperCase and lowerCase letter . That time may you use that
+
+let line = "I am Rakibul Islam";
+console.log(line.search(/rakibul/i));
