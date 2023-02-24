9dea2c5ba0f78981958a6c0974ca802db6d63b5c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-24 23:57:48 +0600

    added replace method

--- Regular expretion/regularExprestion.js
+++ Regular expretion/regularExprestion.js
@@ -6,3 +6,8 @@ console.log(line.search(/rakibul/i));
 let text =
   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi quam inventore rerum, ut, illo delectus fuga ab et at aut dignissimos rem. Qui doloremque culpa nemo. Dignissimos iste quidem iusto blanditiis laborum officiis voluptas animi voluptatum corporis non ratione, repudiandae culpa voluptate quas ducimus et repellat hic dolore ex enim quaerat quia repellendus tempora! Quos minus molestias architecto. Repellendus, est.";
 console.log(text.search(/quas/i));
+
+// some time we have to need replace the any word So that time you can change the word using replace
+
+let text2 = "I love you";
+console.log(text2.replace(/love/i, "hate"));
