cdc5fcb6b7ceeb4232e83a549102264eab882422
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-26 23:27:03 +0600

    added scop in js

--- js errors/errors.js
+++ js errors/errors.js
@@ -1,12 +1,17 @@
 // syntax of error that is normly error handle part here you can use try{} catch(error){} but some time have to need show output like as you like, that time you have to use throw key word
 
+// some time may you use inside of of throw an object
 let x = 12;
 try {
   if (x == "") throw "empty";
   if (isNaN(x)) throw "Not a number";
   x = Number(x);
   if (x < 5) throw "too low";
-  if (x > 10) throw "too low";
+  if (x > 10)
+    throw {
+      message: "Too high number  that is",
+      name: "It is one kind of number ",
+    };
 } catch (error) {
   console.log(error);
 }
---
+++ scop/scop.js
@@ -0,0 +1,9 @@
+// local scop rules
+// code here can NOT use carName
+
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+}
+
+// code here can NOT use carName
