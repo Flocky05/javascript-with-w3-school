87aa062978f8b71f6d3291da140dc02e8c9e7203
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-26 23:36:53 +0600

    added block scop

---
+++ scop/blockScop.js
@@ -0,0 +1,13 @@
+// Block Scope
+// Before ES6 (2015), JavaScript had only Global Scope and Function Scope.
+
+// ES6 introduced two important new JavaScript keywords: let and const.
+
+// These two keywords provide Block Scope in JavaScript.
+
+// Variables declared inside a { } block cannot be accessed from outside the block:
+
+// if any one use let and const inside of second brecate then use it
+{
+  let x = 2;
+}
