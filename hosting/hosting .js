15883905d0a5ec2370f729c76ff5223462b915bf
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-02-28 20:51:14 +0600

    hosting keywords const

--- hosting/hosting .js
+++ hosting/hosting .js
@@ -8,3 +8,12 @@ elem = document.getElementById("demo"); // Find an element
 elem.innerHTML = x; // Display x in the element
 
 var x; // Declare x
+
+// The let and const Keywords
+// Variables defined with let and const are hoisted to the top of the block, but not initialized.
+
+// Meaning: The block of code is aware of the variable, but it cannot be used until it has been declared.
+
+// Using a let variable before it is declared will result in a ReferenceError.
+
+// The variable is in a "temporal dead zone" from the start of the block until it is declared:
