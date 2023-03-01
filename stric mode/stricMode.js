231bc45268ac49d8c348d9057a7ba74b6a2e6fcd
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-01 23:57:00 +0600

    update stric mode

--- stric mode/stricMode.js
+++ stric mode/stricMode.js
@@ -1,3 +1,11 @@
+// The "use strict" directive was new in ECMAScript version 5.
+
+// It is not a statement, but a literal expression, ignored by earlier versions of JavaScript.
+
+// The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+
+// With strict mode, you can not, for example, use undeclared variables.
+
 "use strict";
 x = 3.1416;
 console.log(x);
