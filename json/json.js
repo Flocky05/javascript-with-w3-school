8c1e7f82048735027ef8916b9f2324b1d509513c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-06 23:43:35 +0600

    added json

---
+++ json/json.js
@@ -0,0 +1,9 @@
+// structure of json
+
+/* class ClassName {
+    constructor() { ... }
+} */
+
+// why we use json in the code ?
+
+/* The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects. Since the format is text only, JSON data can easily be sent between computers, and used by any programming language. */
