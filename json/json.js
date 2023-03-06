5c491e35c3d8f3c89745b08c52eca7b0b7d8f1f1
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-06 23:59:31 +0600

    update json

--- json/json.js
+++ json/json.js
@@ -7,3 +7,9 @@
 // why we use json in the code ?
 
 /* The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects. Since the format is text only, JSON data can easily be sent between computers, and used by any programming language. */
+
+/* A JavaScript class is not an object.
+
+It is a template for JavaScript objects. */
+let myCar1 = new Car("Ford", 2014);
+let myCar2 = new Car("Audi", 2019);
