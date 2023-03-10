75f65eb43d096ebae0a459e5de984bb2e245d7df
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-10 12:38:02 +0600

    added this in arrow function

--- arrowFunction/arrowFunction.js
+++ arrowFunction/arrowFunction.js
@@ -10,3 +10,9 @@ hello = () => {
 
 /* If you have parameters, you pass them inside the parentheses: */
 hello = (val) => "Hello " + val;
+
+/* The handling of this is also different in arrow functions compared to regular functions.
+
+In short, with arrow functions there are no binding of this.
+
+In regular functions the this keyword represented the object that called the function, which could be the window, the document, a button or whatever. */
