1353bf58b0f7e700e39b181d4da6a6c216d7b81c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-10 12:39:23 +0600

    example of arrow function

--- arrowFunction/arrowFunction.js
+++ arrowFunction/arrowFunction.js
@@ -16,3 +16,26 @@ hello = (val) => "Hello " + val;
 In short, with arrow functions there are no binding of this.
 
 In regular functions the this keyword represented the object that called the function, which could be the window, the document, a button or whatever. */
+
+// Regular Function:
+hello = function () {
+  document.getElementById("demo").innerHTML += this;
+};
+
+// The window object calls the function:
+window.addEventListener("load", hello);
+
+// A button object calls the function:
+document.getElementById("btn").addEventListener("click", hello);
+
+/* With an arrow function this represents the owner of the function: */
+// Arrow Function:
+hello = () => {
+  document.getElementById("demo").innerHTML += this;
+};
+
+// The window object calls the function:
+window.addEventListener("load", hello);
+
+// A button object calls the function:
+document.getElementById("btn").addEventListener("click", hello);
