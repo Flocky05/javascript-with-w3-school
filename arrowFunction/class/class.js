0e97fdbeb637fd46213c87c0087cb8df7418d569
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-03-12 16:43:14 +0600

    added class

---
+++ arrowFunction/class/class.js
@@ -0,0 +1,19 @@
+/* JavaScript Class Syntax
+Use the keyword class to create a class.
+
+Always add a method named constructor(): */
+
+// syntax:
+
+/* 
+class ClassName {
+  constructor() { ... }
+}
+*/
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
