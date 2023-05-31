796da780fc07ba9d299953274ae4576e32fe28bb
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-01 01:44:40 +0600

    addedJavaScriptRules

---
+++ JavaScriptRules/JavaScriptCodingConventions.js
@@ -0,0 +1,14 @@
+//JavaScript Coding Conventions
+//Variable Names
+// At W3schools we use camelCase for identifier names (variables and functions).
+
+// All names start with a letter.
+
+// At the bottom of this page, you will find a wider discussion about naming rules.
+firstName = "John";
+lastName = "Doe";
+
+price = 19.9;
+tax = 0.2;
+
+fullPrice = price + price * tax;
