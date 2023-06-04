57d90a0e2cfd517a94eb12d5230546817cf486b8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-04 22:22:06 +0600

    JavaScript Objects, Properties, and Methods

--- JavaScript Reserved Words/JavaScriptReservedWords.js
+++ JavaScript Reserved Words/JavaScriptReservedWords.js
@@ -6,3 +6,14 @@ double	final	float	goto
 int	long	native	short
 synchronized	throws	transient	volatile
 */
+
+
+/*
+JavaScript Objects, Properties, and Methods
+You should also avoid using the name of JavaScript built-in objects, properties, and methods:
+Array	Date	eval	function
+hasOwnProperty	Infinity	isFinite	isNaN
+isPrototypeOf	length	Math	NaN
+name	Number	Object	prototype
+String	toString	undefined	valueOf
+&/
