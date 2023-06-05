cbb6c5d2c26aec5c00582caae60b076ddc878a95
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-05 12:44:21 +0600

    Object Literal

--- JavaScript Objects/JavaScriptObjects.js
+++ JavaScript Objects/JavaScriptObjects.js
@@ -23,3 +23,27 @@ Objects are Variables
 JavaScript variables can contain single values:
 */
 let person = "John Doe";
+
+/*
+Using an Object Literal
+This is the easiest way to create a JavaScript Object.
+
+Using an object literal, you both define and create an object in one statement.
+
+An object literal is a list of name:value pairs (like age:50) inside curly braces {}.
+
+The following example creates a new JavaScript object with four properties:
+*/
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+// Spaces and line breaks are not important. An object definition can span multiple lines:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
