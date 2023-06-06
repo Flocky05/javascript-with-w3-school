f41aa74e3462d5122060fba965d0b5922ddfad04
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-06 08:44:22 +0600

    adding Accessing JavaScript Properties

---
+++ JavaScript Best Practices/JavaScriptObjectProperties.js
@@ -0,0 +1,14 @@
+/*
+JavaScript Properties
+Properties are the values associated with a JavaScript object.
+
+A JavaScript object is a collection of unordered properties.
+
+Properties can usually be changed, added, and deleted, but some are read only.
+*/
+// Accessing JavaScript Properties
+objectName.property;
+// or
+objectName["property"];
+// or
+objectName[expression];
