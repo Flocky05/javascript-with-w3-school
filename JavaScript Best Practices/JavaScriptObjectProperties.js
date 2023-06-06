dc45024c99a32ef58ffacb63f33ba5e556a737c5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-06 08:47:06 +0600

    added for...in Loop properties

--- JavaScript Best Practices/JavaScriptObjectProperties.js
+++ JavaScript Best Practices/JavaScriptObjectProperties.js
@@ -12,3 +12,24 @@ objectName.property;
 objectName["property"];
 // or
 objectName[expression];
+
+// may we use them
+person.firstname + " is " + person.age + " years old.";
+
+/*
+JavaScript for...in Loop
+The JavaScript for...in statement loops through the properties of an object.
+Syntax
+for (let variable in object) {
+  // code to be executed
+}
+*/
+const person = {
+  fname: " John",
+  lname: " Doe",
+  age: 25,
+};
+
+for (let x in person) {
+  txt += person[x];
+}
