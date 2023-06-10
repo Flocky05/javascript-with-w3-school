1a88074804cb1c758b5b59cfe6fe3e3fff2a0572
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-10 20:32:20 +0600

    added basic display method

---
+++ Display Objects/DisplayObjects.js
@@ -0,0 +1,11 @@
+/*
+How to Display JavaScript Objects?
+Displaying a JavaScript object will output [object Object].
+*/
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+document.getElementById("demo").innerHTML = person;
