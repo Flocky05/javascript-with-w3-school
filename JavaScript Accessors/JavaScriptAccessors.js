acd0cd69e48ed4bd1bd5697597317217b8948d85
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-11 01:04:43 +0600

    Added JavaScript Setter

--- JavaScript Accessors/JavaScriptAccessors.js
+++ JavaScript Accessors/JavaScriptAccessors.js
@@ -14,3 +14,22 @@ const person = {
 
 // Display data from the object using a getter:
 document.getElementById("demo").innerHTML = person.lang;
+
+/*
+JavaScript Setter (The set Keyword)
+This example uses a lang property to set the value of the language property.
+*/
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "",
+  set lang(lang) {
+    this.language = lang;
+  },
+};
+
+// Set an object property using a setter:
+person1.lang = "en";
+
+// Display data from the object:
+document.getElementById("demo").innerHTML = person1.language;
