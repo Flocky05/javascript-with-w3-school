ddfb812748536817cc02a394022817d387b24246
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-10 20:34:30 +0600

    Using Object.values()

--- Display Objects/DisplayObjects.js
+++ Display Objects/DisplayObjects.js
@@ -39,3 +39,15 @@ for (let x in person3) {
 }
 
 document.getElementById("demo").innerHTML = txt;
+
+/*
+Using Object.values()
+Any JavaScript object can be converted to an array using Object.values():
+*/
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+const myArray = Object.values(person);
