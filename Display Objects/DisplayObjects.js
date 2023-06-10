c11b5adc499ba48d012d3ff89275601e31abb857
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-10 20:33:10 +0600

    Displaying Object Properties

--- Display Objects/DisplayObjects.js
+++ Display Objects/DisplayObjects.js
@@ -9,3 +9,16 @@ const person = {
 };
 
 document.getElementById("demo").innerHTML = person;
+
+/*
+Displaying Object Properties
+The properties of an object can be displayed as a string:
+*/
+const person1 = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+document.getElementById("demo").innerHTML =
+  person1.name + "," + person1.age + "," + person1.city;
