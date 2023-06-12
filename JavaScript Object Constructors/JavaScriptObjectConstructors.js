1f7adfa27a12ad98b6ba5dce0b0db051f1fd03c1
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-12 20:17:35 +0600

    Adding a Property to an Object

--- JavaScript Object Constructors/JavaScriptObjectConstructors.js
+++ JavaScript Object Constructors/JavaScriptObjectConstructors.js
@@ -12,3 +12,23 @@ Objects of the same type are created by calling the constructor function with th
 */
 const myFather = new Person("John", "Doe", 50, "blue");
 const myMother = new Person("Sally", "Rally", 48, "green");
+
+/*
+What is this?
+In JavaScript, the this keyword refers to an object.
+
+Which object depends on how this is being invoked (used or called).
+
+The this keyword refers to different objects depending on how it is used:
+
+In an object method, this refers to the object.
+Alone, this refers to the global object.
+In a function, this refers to the global object.
+In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event.
+Methods like call(), apply(), and bind() can refer this to any object.
+*/
+
+// Adding a Property to an Object
+// Adding a new property to an existing object is easy:
+myFather.nationality = "English";
