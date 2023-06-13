19e4dbc9b8bc45f8e9767f4b5dae592831eb52cd
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-13 16:36:12 +0600

    Adding a Method to a Constructor

--- Adding a Property to a Constructor/AddingPropertyConstructor.js
+++ Adding a Property to a Constructor/AddingPropertyConstructor.js
@@ -12,3 +12,32 @@ function Person(first, last, age, eyecolor) {
   this.eyeColor = eyecolor;
   this.nationality = "English";
 }
+
+/*
+Adding a Method to a Constructor
+Your constructor function can also define methods:
+*/
+function Person1(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.name = function () {
+    return this.firstName + " " + this.lastName;
+  };
+}
+
+/*
+You cannot add a new method to an object constructor the same way you add a new method to an existing object.
+
+Adding methods to an object constructor must be done inside the constructor function:
+*/
+function Person2(firstName, lastName, age, eyeColor) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.eyeColor = eyeColor;
+  this.changeName = function (name) {
+    this.lastName = name;
+  };
+}
