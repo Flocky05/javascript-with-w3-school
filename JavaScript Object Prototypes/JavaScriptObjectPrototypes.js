a1e01bec080bca8986468b1544bd76068990fe9e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-14 11:50:10 +0600

    To add a new property to a constructor

--- JavaScript Object Prototypes/JavaScriptObjectPrototypes.js
+++ JavaScript Object Prototypes/JavaScriptObjectPrototypes.js
@@ -12,3 +12,12 @@ function Person(first, last, age, eyecolor) {
 
 const myFather = new Person("John", "Doe", 50, "blue");
 const myMother = new Person("Sally", "Rally", 48, "green");
+
+//To add a new property to a constructor, you must add it to the constructor function:
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.nationality = "English";
+}
