a50685838fa5d9b02283f10a15b6693eb5f6b6f7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-16 00:15:34 +0600

    Function Rest Parameter

--- Function Definitions/JavaScript Function Parameters/JavaScript FunctionParameters.js
+++ Function Definitions/JavaScript Function Parameters/JavaScript FunctionParameters.js
@@ -26,3 +26,17 @@ function myFunction(x, y) {
 }
 
 // Default Parameter Values
+function myFunction(x, y = 10) {
+  return x + y;
+}
+myFunction(5);
+
+//Function Rest Parameter
+//The rest parameter (...) allows a function to treat an indefinite number of arguments as an array:
+function sum(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
+
+let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
