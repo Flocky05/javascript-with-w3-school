44016017fecf9fb168ab1209ecc72d2c75ffff76
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-15 12:38:49 +0600

    JavaScript Objects vs Maps

--- JavaScriptMap/JSMap.js
+++ JavaScriptMap/JSMap.js
@@ -43,6 +43,16 @@ typeof returns object:
 console.log(typeof fruits);
 
 /*
+JavaScript Objects vs Maps
+Differences between JavaScript Objects and Maps:
+Object                                  	Map
+Not directly iterable	                    Directly iterable
+Do not have a size property             	Have a size property
+Keys must be Strings (or Symbols)       	Keys can be any datatype
+Keys are not well ordered	                Keys are ordered by insertion
+Have default keys	                        Do not have default keys
+/*
+/*
 Map.forEach()
 The forEach() method invokes a callback for each key/value pair in a Map:
 */
