edc492eb1ac91da3af1836a69b6a3005667efedb
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-15 12:29:52 +0600

    added a map in js

---
+++ JavaScriptMap/JSMap.js
@@ -0,0 +1,18 @@
+/*
+A Map holds key-value pairs where the keys can be any datatype.
+
+A Map remembers the original insertion order of the keys.
+
+A Map has a property that represents the size of the map.
+*/
+
+/*
+new Map()
+You can create a Map by passing an Array to the new Map() constructor:
+*/
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200],
+]);
+console.log(fruits);
