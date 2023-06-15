e418a2e3b4f601dd77809101586c7359c57c0779
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-15 18:43:52 +0600

    added arrow function basic

---
+++ Function Definitions/ArrowFunctions.js
@@ -0,0 +1,15 @@
+/*
+Arrow Functions
+Arrow functions allows a short syntax for writing function expressions.
+
+You don't need the function keyword, the return keyword, and the curly brackets.
+
+Example
+// ES5
+var x = function(x, y) {
+  return x * y;
+}
+
+// ES6
+const x = (x, y) => x * y;
+*/
