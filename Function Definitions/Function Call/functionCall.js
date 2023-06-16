b35025871b5422bad7e2a0f69b660df59b04e44e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-16 10:11:46 +0600

    The JavaScript call() Method

--- Function Definitions/Function Call/functionCall.js
+++ Function Definitions/Function Call/functionCall.js
@@ -16,3 +16,27 @@ const person = {
 
 // This will return "John Doe":
 person.fullName();
+
+/*
+The JavaScript call() Method
+The call() method is a predefined JavaScript method.
+
+It can be used to invoke (call) a method with an owner object as an argument (parameter).
+
+With call(), an object can use a method belonging to another object.
+*/
+
+const personX = {
+  fullName: function () {
+    return this.fristName + " " + this.lastName;
+  },
+};
+const person1 = {
+  fristName: "Rakibul",
+  lastName: "Islam",
+};
+const person2 = {
+  fristName: "Sakib Al",
+  lastName: "Hasan",
+};
+console.log(personX.fullName.call(person2));
