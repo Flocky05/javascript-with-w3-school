862c2a47360d4ce18d947d10d9282a09c4b724b7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-16 09:17:31 +0600

    added global object

--- Function Definitions/Function Invocation/functionInvocation.js
+++ Function Definitions/Function Invocation/functionInvocation.js
@@ -12,3 +12,17 @@ function myFunction(a, b) {
   return a * b;
 }
 console.log(myFunction(10, 2));
+
+/*
+The Global Object
+When a function is called without an owner object, the value of this becomes the global object.
+
+In a web browser the global object is the browser window.
+
+This example returns the window object as the value of this:
+*/
+let x = myFunction(); // x will be the window object
+
+function myFunction() {
+  return this;
+}
