83256a3b46029c20c4957f409c73574da65055e9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-16 09:14:48 +0600

    added basic function invocation

---
+++ Function Definitions/Function Invocation/functionInvocation.js
@@ -0,0 +1,14 @@
+/*
+Invoking a JavaScript Function
+The code inside a function is not executed when the function is defined.
+
+The code inside a function is executed when the function is invoked.
+
+It is common to use the term "call a function" instead of "invoke a function".
+
+It is also common to say "call upon a function", "start a function", or "execute a function".
+*/
+function myFunction(a, b) {
+  return a * b;
+}
+console.log(myFunction(10, 2));
