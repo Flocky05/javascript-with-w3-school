4fae31b569758552f879edd03797d9d8acae2ca0
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-18 10:25:08 +0600

    A Counter Dilemma basic

--- Function Definitions/JavaScript Closures/JavaScriptClosures.js
+++ Function Definitions/JavaScript Closures/JavaScriptClosures.js
@@ -19,3 +19,24 @@ Global variables live until the page is discarded, like when you navigate to ano
 
 Local variables have short lives. They are created when the function is invoked, and deleted when the function is finished.
 */
+
+/*
+A Counter Dilemma
+Suppose you want to use a variable for counting something, and you want this counter to be available to all functions.
+
+You could use a global variable, and a function to increase the counter:
+*/
+// Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+// The counter should now be 3
