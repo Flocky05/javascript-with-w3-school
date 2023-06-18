d52f3d928a666d59dab32e1a8babbde3b0b968fb
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-18 10:54:15 +0600

    JavaScript Nested Functions

--- Function Definitions/JavaScript Closures/JavaScriptClosures.js
+++ Function Definitions/JavaScript Closures/JavaScriptClosures.js
@@ -40,3 +40,22 @@ add();
 add();
 
 // The counter should now be 3
+
+/*
+JavaScript Nested Functions
+All functions have access to the global scope.  
+
+In fact, in JavaScript, all functions have access to the scope "above" them.
+
+JavaScript supports nested functions. Nested functions have access to the scope "above" them.
+
+In this example, the inner function plus() has access to the counter variable in the parent function:
+*/
+function add() {
+  let counter = 0;
+  function plus() {
+    counter += 1;
+  }
+  plus();
+  return counter;
+}
