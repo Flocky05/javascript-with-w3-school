60033b428d5ab8c636d6151f994f7cdc12d8ffeb
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-20 00:03:12 +0600

    added dom append

---
+++ Dom/Dom Append/DomAppend.js
@@ -0,0 +1,7 @@
+const fruitesName = document.getElementById("fruits-list");
+const li = document.createElement("li");
+li.innerText = "Litchi";
+fruitesName.appendChild(li);
+const li1 = document.createElement("li1");
+li1.innerText = "plum";
+fruitesName.appendChild(li1);
---
+++ Dom/Dom.js
@@ -0,0 +1,1 @@
+console.log("I don't know you man , who are you?");
