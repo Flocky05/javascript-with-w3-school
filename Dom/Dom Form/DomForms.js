aff8d6e57a212065c0ceef2cb54544cd6853aa7d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-23 00:24:34 +0600

    JavaScript Form Validation

---
+++ Dom/Dom Form/DomForms.js
@@ -0,0 +1,13 @@
+/*
+JavaScript Form Validation
+HTML form validation can be done by JavaScript.
+
+If a form field (fname) is empty, this function alerts a message, and returns false, to prevent the form from being submitted:
+*/
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
