b74a47aa17bec51179292b9f7046b5f1f466d6f9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-23 22:12:18 +0600

    added basic system style of css in Dom

---
+++ Dom/Dom Css/DomCss.js
@@ -0,0 +1,8 @@
+/*
+The HTML DOM allows JavaScript to change the style of HTML elements.
+
+Changing HTML Style
+To change the style of an HTML element, use this syntax:
+
+document.getElementById(id).style.property = new style
+*/
