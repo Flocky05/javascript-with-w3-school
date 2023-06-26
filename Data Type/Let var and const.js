974b1dfc8d22dc04c4de3e28d87358ca25b04556
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-26 10:26:15 +0600

    added let basic

--- Data Type/Let var and const.js
+++ Data Type/Let var and const.js
@@ -5,13 +5,34 @@ Before the advent of ES6, var declarations ruled. There are issues associated wi
 var variables can be re-declared and updated
 This means that we can do this within the same scope and won't get an error.
 */
-var greeter = "hey hi";
-var greeter = "say Hello instead";
-console.log(greeter);
+// var greeter = "hey hi";
+// var greeter = "say Hello instead";
+// console.log(greeter);
 
 /*
 Hoisting of var
 Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution. This means that if we do this:
 */
-console.log(greeter);
-var greeter = "say hello";
+// console.log(greeter);
+// var greeter = "say hello";
+
+/*
+Let
+let is now preferred for variable declaration. It's no surprise as it comes as an improvement to var declarations. It also solves the problem with var that we just covered. Let's consider why this is so.
+
+let is block scoped
+A block is a chunk of code bounded by {}. A block lives in curly braces. Anything within curly braces is a block.
+*/
+let greeting1 = "say Hi";
+let times = 4;
+
+if (times > 2) {
+  let hello = "say Hello instead";
+  console.log(greeting1); // "say Hello instead"
+}
+console.log(greeting1); // hello is not defined
+
+/*
+Hoisting of let
+Just like  var, let declarations are hoisted to the top. Unlike var which is initialized as undefined, the let keyword is not initialized. So if you try to use a let variable before declaration, you'll get a Reference Error.
+*/
