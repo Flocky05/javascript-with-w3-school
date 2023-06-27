6dddd59d13927c4c73e1da0e5f373bfb73ce6f5e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-27 23:28:12 +0600

    added bom basic

---
+++ Bom/Bom.js
@@ -0,0 +1,6 @@
+/*
+The Browser Object Model (BOM)
+There are no official standards for the Browser Object Model (BOM).
+
+Since modern browsers have implemented (almost) the same methods and properties for JavaScript interactivity, it is often referred to, as methods and properties of the BOM.
+*/
