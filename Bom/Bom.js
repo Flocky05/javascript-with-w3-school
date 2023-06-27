713844a57dedb30996fa37aa7728279acfa869b7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-06-27 23:28:56 +0600

    added window Object

--- Bom/Bom.js
+++ Bom/Bom.js
@@ -4,3 +4,21 @@ There are no official standards for the Browser Object Model (BOM).
 
 Since modern browsers have implemented (almost) the same methods and properties for JavaScript interactivity, it is often referred to, as methods and properties of the BOM.
 */
+
+/*
+The Window Object
+The window object is supported by all browsers. It represents the browser's window.
+
+All global JavaScript objects, functions, and variables automatically become members of the window object.
+
+Global variables are properties of the window object.
+
+Global functions are methods of the window object.
+
+Even the document object (of the HTML DOM) is a property of the window object:
+
+window.document.getElementById("header");
+is the same as:
+
+document.getElementById("header");
+*/
