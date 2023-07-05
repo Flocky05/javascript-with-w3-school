04862eb3c2ee9302ed85ac311a7cd1a8ae53732a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-05 23:37:20 +0600

    added Modern Browsers

--- AJAX/AJAX introduction/AJAXIntroduction.js
+++ AJAX/AJAX introduction/AJAXIntroduction.js
@@ -17,3 +17,13 @@ AJAX just uses a combination of:
 A browser built-in XMLHttpRequest object (to request data from a web server)
 JavaScript and HTML DOM (to display or use the data)
 */
+
+/*
+Modern Browsers (Fetch API)
+Modern Browsers can use Fetch API instead of the XMLHttpRequest Object.
+
+The Fetch API interface allows web browser to make HTTP requests to web servers.
+
+If you use the XMLHttpRequest Object, Fetch can do the same in a simpler way.
+
+*/
