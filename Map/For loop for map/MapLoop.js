f893e26972693a2360c184840e9497a6acfd3e72
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-06 13:54:51 +0600

    added array maping

--- Map/For loop for map/MapLoop.js
+++ Map/For loop for map/MapLoop.js
@@ -1,7 +1,17 @@
 let numbers = [1, 2, 3, 6, 4, 3, 2];
 let output = [];
+let output1 = [];
 for (const number of numbers) {
   const double = number * 2;
   output.push(double);
 }
-console.log(output);
+//console.log(output);
+
+function getTriple(numbers) {
+  for (const number of numbers) {
+    const triple = number * 3;
+    output1.push(triple);
+  }
+  return output1;
+}
+console.log(getTriple(numbers));
