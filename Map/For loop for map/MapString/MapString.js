3b697a9960defc027f13c58429e0917531ce67c5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-06 15:36:29 +0600

    added map in object

--- Map/For loop for map/MapString/MapString.js
+++ Map/For loop for map/MapString/MapString.js
@@ -1,6 +1,6 @@
 const friends = [
   "Md Mehedi Ahsan",
-  "Md Shohanur Rhaman Emon",
+  "Shohanur Rhaman Emon",
   "Abu Siam",
   "Nawaz Talukder Songlap",
   "Rifat Hussain",
@@ -10,4 +10,25 @@ const friends = [
 ];
 
 const freindLength = friends.map((friend) => friend.length);
-console.log(freindLength);
+//console.log(freindLength);
+
+const friendFristLetter = friends.map((friend) => friend[0]);
+//console.log(friendFristLetter);
+
+const numbers = [10, 25, 12, 65, 25, 56, 85];
+
+const numberAdd = numbers.map((x) => x + 3);
+//console.log(numberAdd);
+
+const products = [
+  { id: 1, name: "Mobail", price: 40000 },
+  { id: 1, name: "Watch", price: 20000 },
+  { id: 1, name: "Tablet", price: 23000 },
+  { id: 1, name: "shoo", price: 90000 },
+];
+
+//const productsName = products.map((product) => product.name);
+//console.log(producsName);
+
+const productPrice = products.map((product) => product.price);
+console.log(productPrice);
