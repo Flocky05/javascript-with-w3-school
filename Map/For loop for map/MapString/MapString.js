71260beaad87c70f334abd6d4028714a34249428
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-06 15:06:42 +0600

    added map with string

---
+++ Map/For loop for map/MapString/MapString.js
@@ -0,0 +1,13 @@
+const friends = [
+  "Md Mehedi Ahsan",
+  "Md Shohanur Rhaman Emon",
+  "Abu Siam",
+  "Nawaz Talukder Songlap",
+  "Rifat Hussain",
+  "Abraham Khan Shanto",
+  "Saif Sadi Akash",
+  "Atahar Ali ",
+];
+
+const freindLength = friends.map((friend) => friend.length);
+console.log(freindLength);
