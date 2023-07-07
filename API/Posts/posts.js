9cedacd0dcc5e4ba6e9319370d63643319e2ab8d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-08 00:02:01 +0600

    added a patch method

--- API/Posts/posts.js
+++ API/Posts/posts.js
@@ -52,3 +52,17 @@ function updateApost() {
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
+
+function patchApost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
