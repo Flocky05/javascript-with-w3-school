fc88c452d67c3aace19802193b0e1535c4f695cc
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-08 00:00:35 +0600

    added create a post

--- API/Posts/posts.js
+++ API/Posts/posts.js
@@ -19,3 +19,19 @@ function displayData(posts) {
     postContainer.appendChild(postDiv);
   }
 }
+
+function createApost() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
