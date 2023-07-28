6d52ee2c5f2f0e8e6bd8f975446d94a1207ea86f
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-28 06:28:54 +0600

    jQuary vs javascript

---
+++ J Quary/jQuary.js
@@ -0,0 +1,9 @@
+// why use j quary
+// jQuery vs JavaScript
+// jQuery was created in 2006 by John Resig. It was designed to handle Browser Incompatibilities and to simplify HTML DOM Manipulation, Event Handling, Animations, and Ajax.
+
+// syntax of jQuary
+const myElement = $("#202");
+
+// but in js we use
+myElement = document.getElementById("id01");
