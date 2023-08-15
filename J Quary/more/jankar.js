50860de89a1bca65fd2148773583e2866a39e221
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-16 02:44:01 +0600

    replace the element of array

--- J Quary/more/jankar.js
+++ J Quary/more/jankar.js
@@ -52,5 +52,11 @@ var fruits = ['Apple', 'Banana', 'Orange'];
 a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’.
 b) Remove ‘Orange’ and add ‘Watermelon’. */
 
-var fruits = ["Apple", "Banana", "Orange"];
-console.log(fruits.indexOf("Banana"));
+let fruits = ["Apple", "Banana", "Orange"];
+const numberBanana = fruits.indexOf("Banana");
+console.log(numberBanana);
+
+if (numberBanana != -1) {
+  fruits[numberBanana] = "mango";
+}
+console.log(fruits);
