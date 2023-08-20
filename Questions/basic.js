096a941960e3094ecd2df003c76216fb0eabfcac
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-20 16:55:55 +0600

    find the array element length

--- Questions/basic.js
+++ Questions/basic.js
@@ -120,3 +120,31 @@ const add = (a, b = 0) => {
 };
 const result1 = add(12, 67);
 console.log(result1);
+
+/* Write an arrow function where it will do the following:
+a) It will take an array where the array elements will be the
+name of your friends
+b) Check if the length of each element is even, push elements
+with even length to a new array and return the result
+Print the result. */
+
+const friends = [
+  "Mehedi",
+  "Emon",
+  "Siam",
+  "Shanto",
+  "Krishno",
+  "Akash",
+  "Pulok",
+  "Songlap",
+];
+const evenName = [];
+const allFriends = () => {
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].length % 2 == 0) {
+      evenName.push(friends[i]);
+    }
+  }
+};
+console.log(allFriends());
+console.log(evenName);
