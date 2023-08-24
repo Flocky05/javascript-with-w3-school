7111d0270ba4615e5445385035b2486574929230
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-24 09:28:43 +0600

    avg from an array

--- Questions/array.js
+++ Questions/array.js
@@ -19,3 +19,16 @@ if (a < b && a < c) {
 } else if (b < a && b < c) {
   console.log("The smallest number is ", b);
 } else console.log("The smallest number is ", c);
+
+/* ৩. একটা ফাংশন লিখো। সেই ফাংশনের মধ্যে ইনপুট হিসেবে একটা array নিবে। সেই array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হবে ইনপুট নেয়া array এর মধ্যে যতগুলা সংখ্যা আছে। সেই সংখ্যা গুলার গড় বের করবে। তারপর সেই গড় ফাংশনের রিটার্ন হিসেবে দিয়ে দিবে। একটু চিন্তা করো। বুঝার চেষ্টা করো। ট্রাই করো। দেখো পারো কিনা।  */
+let avgN = 0;
+let sum = 0;
+function avg(array) {
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  avgN = sum / array.length;
+  return avgN;
+}
+avg([1, 2, 3, 4, 5, 6]);
+console.log(avgN);
