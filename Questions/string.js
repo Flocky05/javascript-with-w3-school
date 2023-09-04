c7cc0da2f18536ec85eec928d7d69fe2779e8d3d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-04 10:18:25 +0600

    make friends name lowerCase to UperCase

--- Questions/string.js
+++ Questions/string.js
@@ -8,3 +8,21 @@ function reverseWords(str) {
 }
 let str = "my name is rakibul islam";
 console.log(reverseWords(str));
+
+/* make a word frist letter is uper case and other letters are lower case */
+let friends = [
+  "mehDi",
+  "EmoN",
+  "siAm",
+  "krishno",
+  "shanTo",
+  "Akash",
+  "sonGLap",
+  "PuloK",
+];
+for (let i = 0; i < friends.length; i++) {
+  let friend = friends[i].toLowerCase();
+  let FristLetter = friend.slice(0, 1).toUpperCase();
+  let FullName = FristLetter + friend.slice(1, friend.length);
+  console.log(FullName);
+}
