2a6c18aa2c623810a5d6ef0c4032e54bf459812b
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-04 11:38:37 +0600

    leap year function with array

--- Questions/To do list problem.js
+++ Questions/To do list problem.js
@@ -6,3 +6,20 @@ function HourCoverter(time) {
   return minutes;
 }
 console.log(HourCoverter(10));
+
+/* Write a function findLeapYear() that will take the array
+[2023,2024,2025,2028,2030] as the input parameter and will check if
+each year is a leap year. If a year is a leap year insert that year in a
+new array, return the new array and print the result. */
+
+let years = [2023, 2024, 2025, 2028, 2030];
+let leapYear = [];
+function findLeapYear(years) {
+  for (let i = 0; i < years.length; i++) {
+    if ((years[i] % 4 == 0 && years[i] % 100 != 0) || years[i] % 400 == 0) {
+      leapYear.push(years[i]);
+    }
+  }
+  return leapYear;
+}
+console.log(findLeapYear(years));
