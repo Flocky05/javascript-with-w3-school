7ab2ee1af466a614fc5f9b01ecaa0a6a8a85af60
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-07 00:02:31 +0600

    added a number section

--- Questions/To do list problem.js
+++ Questions/To do list problem.js
@@ -89,3 +89,41 @@ console.log(isPrimeNumber(33));
 
 let name = "what the hell bro?";
 console.log(name.splice());
+/* 
+// JavaScript Program to convert decimal number to
+// roman numerals */
+function printRoman(number) {
+  let num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let sym = [
+    "I",
+    "IV",
+    "V",
+    "IX",
+    "X",
+    "XL",
+    "L",
+    "XC",
+    "C",
+    "CD",
+    "D",
+    "CM",
+    "M",
+  ];
+  let i = 12;
+  while (number > 0) {
+    let div = Math.floor(number / num[i]);
+    number = number % num[i];
+    while (div--) {
+      document.write(sym[i]);
+    }
+    i--;
+  }
+}
+
+//Driver program
+
+let number = 3549;
+
+printRoman(number);
+
+//This code is contributed by Manoj
